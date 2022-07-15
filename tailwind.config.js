6414a6a1ea516fd6da8e739816d87a196dd3007a
Karan Kapoor <kk8658141@gmail.com>
2022-07-15 16:11:14 +0530

    resolve the errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  // ...
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
